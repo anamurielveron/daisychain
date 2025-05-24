f482d2a8e380dca7c6e4ad47e44bf4f9e9fc201e
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2025-05-24 15:31:18 +0800

    Changed struct to class

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,72 @@ using namespace std;
 * TEMPLATE COMMAND FUNCTIONS
 */
 
+<<<<<<< Updated upstream
+=======
+// Session struct to hold screen info
+class Session {
+	string name;
+	string currentLine;
+	int totalLines=0;
+	string timestamp;
+
+	public:
+		void newSession(string scrName, string timeCreated) {
+			name = scrName;
+			currentLine = "";
+			timestamp = timeCreated;
+		}
+
+		void screen();
+
+		string GetName() {
+			return name;
+		}
+};
+
+Session sessions[10];
+
+>>>>>>> Stashed changes
 void initialize() {
 	printColor("\"initialize\" command recognized. Doing something...\n", YELLOW);
 	// TODO: Implement the initialize command
 }
 
+<<<<<<< Updated upstream
 void screen() {
 	printColor("\"screen\" command recognized. Doing something...\n", YELLOW);
 	// TODO: Implement the screen command
+=======
+void Session::screen() {
+	//printColor("\"screen\" command recognized. Doing something...\n", YELLOW);
+	// TODO: Implement the screen command
+
+	//Display session name and time created
+	printColor(name + "\n\n", YELLOW);
+	printColor(timestamp + "\n", YELLOW);
+	cout << "Total commands happened on screen: " << totalLines << "\n\n";
+	cout << "Previous command done: " << currentLine << "\n\n";
+
+	printPlaceHolderConsoles();
+	while (true) {
+		std::string command;
+		printColor("~> ", CYAN);
+		std::getline(std::cin, command);
+		if (command == "X") {
+			printColor("Leaving Screen...\n", RED);
+			break;
+		}
+		else {
+			printColor("Doing something \n", YELLOW);
+			totalLines++;
+			currentLine = command;
+		}
+	}
+	system("cls");
+
+	printBanner();
+	printSubtitle();
+>>>>>>> Stashed changes
 }
 
 void schedulerTest() {
@@ -80,10 +138,102 @@ int main()
 	printBanner();
 	printSubtitle();
 
+<<<<<<< Updated upstream
 	while (true) {
 		std::string command;
 		printColor("~> ", CYAN);
 		std::getline(std::cin, command);
+=======
+	//Holds the index for which session to resume
+	int sessionToResume = 0;
+
+	bool screenFound = false;
+	
+	while (true) 
+	{
+		// Print welcome banner
+		printBanner();
+		printSubtitle();
+
+		while (true) {
+			screenFound = false;
+			std::string command;
+			printColor("~> ", CYAN);
+			std::getline(std::cin, command);
+
+			if (command == "help") {
+				printHelp();
+			}
+			else if (command == "initialize") {
+				initialize();
+			}
+			else if (command.find("screen") != string::npos) {
+				if (command.find("-s") != string::npos) {
+					
+					//Checks if session name already exists
+					for (Session session : sessions) {
+						if (session.GetName() == command.substr(command.find("-s") + 3)) {
+							screenFound = true;
+							break;
+						}
+					}
+					
+					if (!screenFound) {
+						//If session name does not exist, create new session
+						sessions[currentSessionCount].newSession(command.substr(command.find("-s") + 3), getCurrentTimestamp());
+						system("cls");
+						sessions[currentSessionCount].screen();
+						currentSessionCount++;
+					}
+					else {
+						printColor("Screen already exists...\n", MAGENTA);
+					}
+				}
+				else if (command.find("-r") != string::npos) {
+					sessionToResume = 0;
+
+					//Checks if session name already exists
+					for (Session session : sessions) {
+						if (session.GetName() == command.substr(command.find("-r") + 3)) {
+							screenFound = true;
+							break;
+						}
+						sessionToResume++;
+					}
+
+					if (screenFound) {
+						//If session name exists, resume session
+						system("cls");
+						sessions[sessionToResume].screen();
+					}
+					else {
+						printColor("Screen does not exist...\n", MAGENTA);
+					}
+				}
+				else {
+					printColor("Screen command not recognized....\n", RED);
+				}
+			}
+			else if (command == "scheduler-test") {
+				schedulerTest();
+			}
+			else if (command == "scheduler-stop") {
+				schedulerStop();
+			}
+			else if (command == "report-util") {
+				reportUtil();
+			}
+			else if (command == "clear") {
+				clear();
+			}
+			else if (command == "exit") {
+				printColor("Exiting...\n", RED);
+				break;
+			}
+			else {
+				printColor("Unknown command. Type 'help' for a list of commands.\n", RED);
+			}
+>>>>>>> Stashed changes
 
 		if (command == "help") {
 			printHelp();
